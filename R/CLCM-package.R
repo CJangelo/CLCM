17d8866cf9d22c07445c0d25608e8db5bea3ad12
CJangelo <charles.iaconangelo@gmail.com>
2026-06-10 19:51:45 -0400

    package documentation

---
+++ R/CLCM-package.R
@@ -0,0 +1,6 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom stats as.formula complete.cases dbeta optim pbeta rbeta model.matrix var dnorm runif rnorm sd pchisq
+#' @importFrom utils combn head
+NULL
